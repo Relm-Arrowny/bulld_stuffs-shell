236716d8bf0701899610e86fbf67bcc980c525d2
Relm Arrown <ferimons@hotmail.com>
2026-08-18 07:15:55 +0000

    codecrafters submit [skip ci]

--- src/helper.c
+++ src/helper.c
@@ -1,14 +1,15 @@
 
 #include "helper.h"
-char commands[] = "type echo exit";
+const char *builtins[] = {"exit", "echo", "type"};
 
 int checkType(char* input){
-    char arg[100];
-    strcpy(arg,input);
-    arg[strcspn(arg, "\n")] = '\0';
-    char *res = strstr(commands, arg);
-    if (res != NULL) return 1;
-    else return 0;
+    size_t count = sizeof(builtins) / sizeof(builtins[0]);
+
+    for (size_t i = 0; i < count; i++){
+        if (strcmp(input, builtins[i]) == 0) 
+            return 1;
+    }
+    return 0;
 }
 
 void noCommand(char* com){
