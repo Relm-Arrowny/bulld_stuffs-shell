3378f01274a1e2f140e1faf0647662b63dbce0e4
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-08-28 01:35:53 +0000

    codecrafters submit [skip ci]

--- src/helper.c
+++ src/helper.c
@@ -18,9 +18,10 @@ char *checkTypePath(const char* path, const char* input){
         
         char full_path[1024];
         snprintf(full_path, sizeof(full_path), "%s/%s", *ptr, input);
-        if (is_executable(full_path))
+        if (access(full_path, X_OK) == 0){
             printf("%s is %s\n", input,full_path);
             return "d";
+            }
     }
         
     free_string_list(path_list);
