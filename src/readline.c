43b61a266e0004aab36349582a9cb167626bfb47
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-09-10 21:56:01 +0000

    codecrafters submit [skip ci]

--- src/readline.c
+++ src/readline.c
@@ -61,7 +61,119 @@ void raw_mode(){
 }
 
 
+static void handle_tab(char *buffer, int *len, char last_char){
+    if (len == 0){
+        printf("\a");
+        fflush(stdout);
+        return;
+    }
 
+    StringList matches;
+    init_string_list(&matches);
+    if (matches.items == NULL) {
+        return;
+    }
+
+    // split the command when there is space
+    char *last_space = strrchr(buffer, ' ');
+    char *prefix = (last_space != NULL) ? last_space + 1 : buffer;
+    int prefix_len = strlen(prefix);
+
+    if (last_space == NULL){
+        //builtins
+        for (int i = 0; builtins[i] != NULL; i++) {
+            if (strncmp(builtins[i], prefix, prefix_len) == 0) {
+                add_to_string_list(&matches, builtins[i] );
+            }
+            
+        }
+        //in path
+        char *path_env = getenv("PATH");
+        if (path_env == NULL || *path_env == '\0') {
+            char ** path_list = split_string(path_env, ":");      
+            for (int i = 0; path_list[i] !=NULL;i++){
+                DIR* directory = opendir(path_list[i]);
+                if (directory == NULL) {
+                    continue;
+                }                   
+                struct dirent* entry = NULL;
+                while ((entry = readdir(directory)) != NULL) {
+                    if (strncmp(entry->d_name, prefix, prefix_len) == 0) {
+                        add_to_string_list(&matches,entry->d_name);
+                    }
+                }
+                closedir(directory);
+            }
+            free_string_list(path_list);
+        }
+    }
+    else{
+        char *last_slash = strrchr(prefix, '/');
+        char *path_prefix = (last_slash != NULL) ? last_slash + 1 : prefix;
+        int path_prefix_len = strlen(path_prefix);
+        DIR *directory = NULL;
+
+        if (last_slash == NULL){
+            directory = opendir(".");
+
+        }
+        else{
+            int dir_len = (int)(last_slash - prefix);
+            char *dir_path = (dir_len == 0) ? strdup("/") : strndup(prefix, dir_len);
+            directory = opendir(dir_path);
+            free(dir_path);
+        }    
+        if (directory != NULL) {
+            struct dirent *entry;
+            while ((entry = readdir(directory)) != NULL) {
+                if (entry->d_name[0] == '.' && path_prefix[0] != '.') {
+                    continue;
+                }
+                if (strncmp(entry->d_name, path_prefix, path_prefix_len) == 0) {
+                    add_to_string_list(&matches,entry->d_name);
+                }
+            }
+            closedir(directory);
+            prefix_len = path_prefix_len;
+        }
+
+    }
+    rm_dup_string_list(&matches);
+    fflush(stdout);
+    if (matches.count==1){
+        output_match_command(buffer, len,  matches.items[0],prefix_len);
+    }
+    else if ( matches.count>1 && last_char != '\t'){
+        int common_len = 0;
+        char *partial_match;
+        partial_match = malloc(4096);
+        if (partial_match != NULL) {
+            while ( matches.items[0][common_len] != '\0' &&
+                matches.items[0][common_len] ==  matches.items[ matches.count - 1][common_len]) {
+                partial_match[common_len] = matches.items[0][common_len];
+                common_len++;
+            }
+            partial_match[common_len] = '\0';
+            printf("\a");
+            fflush(stdout);
+            output_partial_match_command(buffer, len, partial_match,prefix_len);
+            free(partial_match);
+        }
+    }
+    else if( matches.count>1 && last_char == '\t') {
+        printf("\n");
+        for (int i = 0; matches.items[i] != NULL; i++){
+            printf("%s ", matches.items[i]);
+        }
+        printf("\n$ %s",buffer);
+        fflush(stdout);
+    }
+    else{
+        printf("\a");
+        fflush(stdout);
+    }
+    free_string_list(matches.items);          
+}
 
 char *readline(const char *prompt) {
     raw_mode();
@@ -101,119 +213,8 @@ char *readline(const char *prompt) {
         }
         // handle tab
         if (c == '\t') {
-
-            if (len == 0){
-                printf("\a");
-                fflush(stdout);
-                continue;
-            }
-
-            StringList matches;
-            init_string_list(&matches);
-            if (matches.items == NULL) {
-                return NULL;
-            }
-
-            // split the command when there is space
-            char *last_space = strrchr(buffer, ' ');
-            char *prefix = (last_space != NULL) ? last_space + 1 : buffer;
-            int prefix_len = strlen(prefix);
-
-            if (last_space == NULL){
-                //builtins
-                for (int i = 0; builtins[i] != NULL; i++) {
-                    if (strncmp(builtins[i], prefix, prefix_len) == 0) {
-                        add_to_string_list(&matches, builtins[i] );
-                    }
-                    
-                }
-                //in path
-                char *path_env = getenv("PATH");
-                if (path_env == NULL || *path_env == '\0') {
-                    char ** path_list = split_string(path_env, ":");      
-                    for (int i = 0; path_list[i] !=NULL;i++){
-                        DIR* directory = opendir(path_list[i]);
-                        if (directory == NULL) {
-                            continue;
-                        }                   
-                        struct dirent* entry = NULL;
-                        while ((entry = readdir(directory)) != NULL) {
-                            if (strncmp(entry->d_name, prefix, prefix_len) == 0) {
-                                add_to_string_list(&matches,entry->d_name);
-                            }
-                        }
-                        closedir(directory);
-                    }
-                    free_string_list(path_list);
-                }
-            }
-            else{
-                char *last_slash = strrchr(prefix, '/');
-                char *path_prefix = (last_slash != NULL) ? last_slash + 1 : prefix;
-                int path_prefix_len = strlen(path_prefix);
-                DIR *directory = NULL;
-
-                if (last_slash == NULL){
-                    directory = opendir(".");
-
-                }
-                else{
-                    int dir_len = (int)(last_slash - prefix);
-                    char *dir_path = (dir_len == 0) ? strdup("/") : strndup(prefix, dir_len);
-                    directory = opendir(dir_path);
-                    free(dir_path);
-                }    
-                if (directory != NULL) {
-                    struct dirent *entry;
-                    while ((entry = readdir(directory)) != NULL) {
-                        if (entry->d_name[0] == '.' && path_prefix[0] != '.') {
-                            continue;
-                        }
-                        if (strncmp(entry->d_name, path_prefix, path_prefix_len) == 0) {
-                            add_to_string_list(&matches,entry->d_name);
-                        }
-                    }
-                    closedir(directory);
-                    prefix_len = path_prefix_len;
-                }
-
-            }
-            rm_dup_string_list(&matches);
-            fflush(stdout);
-            if (matches.count==1){
-                output_match_command(buffer, &len,  matches.items[0],prefix_len);
-            }
-            else if ( matches.count>1 && last_char != '\t'){
-                int common_len = 0;
-                char *partial_match;
-                partial_match = malloc(4096);
-                if (partial_match != NULL) {
-                    while ( matches.items[0][common_len] != '\0' &&
-                        matches.items[0][common_len] ==  matches.items[ matches.count - 1][common_len]) {
-                        partial_match[common_len] = matches.items[0][common_len];
-                        common_len++;
-                    }
-                    partial_match[common_len] = '\0';
-                    printf("\a");
-                    fflush(stdout);
-                    output_partial_match_command(buffer, &len, partial_match,prefix_len);
-                    free(partial_match);
-                }
-            }
-            else if( matches.count>1 && last_char == '\t') {
-                printf("\n");
-                for (int i = 0; matches.items[i] != NULL; i++){
-                    printf("%s ", matches.items[i]);
-                }
-                printf("\n$ %s",buffer);
-                fflush(stdout);
-            }
-            else{
-                printf("\a");
-                fflush(stdout);
-            }
-            free_string_list(matches.items);
-            last_char = c;
+            handle_tab(buffer, &len, last_char);
+            last_char = '\t';
             continue;
         }
         if (c != '\r' && c != '\n' ) {
@@ -229,3 +230,6 @@ char *readline(const char *prompt) {
     cooked_mode();
     return buffer;
 }
+
+
+          
